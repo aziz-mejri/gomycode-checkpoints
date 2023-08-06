50a5fe743fb2848b3882a802e72cc541228ed4e4
Aymen Jemi <azizmejri905@gmail.com>
2023-08-06 18:36:15 +0100

    add react state project

---
+++ React_State/src/App.js
@@ -0,0 +1,51 @@
+import { Component } from 'react';
+import img  from './img.jpg';
+
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      //number:1,
+      show : true,
+      Person : { 
+                fullName : "Aziz Mejri",
+                bio : "Technicien en informatique", 
+                imgSrc: img, 
+                profession:"IT"
+              },
+    }
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+  
+
+
+  clickHandler(){
+    this.setState(function (prevState){
+      return{
+        show : !prevState.show ,
+      }
+    });
+  }
+
+  render(){
+    if(this.state.show)
+    return (
+      <div className="container">
+        <button className="btn btn-primary" onClick={this.clickHandler}>Hide</button>
+        <br/>
+        <img src={img} alt="image peronel" width="400" height="430" />
+        <h1>{this.state.Person.fullName}</h1>
+        <h2>{this.state.Person.bio}</h2>
+        <h2>{this.state.Person.profession}</h2>
+      </div>
+      );
+      else return (
+        <div className="container">
+      <button className="btn btn-primary" onClick={this.clickHandler}>Show</button>
+      </div>
+      );
+  }
+}
+
+export default App;
